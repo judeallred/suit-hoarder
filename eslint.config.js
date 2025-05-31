ce39d0f49aa129ca58aa9a5231c8b7f16762c8ca
Jude Allred <jude@allred.nyc>
2025-05-31 02:53:59 -0400

    re-add html and css linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import prettier from 'eslint-config-prettier/flat';
+import html from 'eslint-plugin-html';
+import css from 'eslint-plugin-css';
 
 export default [
   js.configs.recommended,
   prettier,
   {
+    files: ["www/**/*.js"],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -19,6 +22,23 @@ export default [
       'no-console': 'warn',
       'no-undef': 'error',
       'semi': ['error', 'always']
+    }
+  },
+  {
+    files: ["www/**/*.html"],
+    plugins: {
+      html
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+  {
+    files: ["www/**/*.css"],
+    plugins: {
+      css
     }
   }
 ]; 
